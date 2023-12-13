63905644037e3839d8420599efe5e2a98ddfd70c
Oliver Berggren <oliverberggren00@gmail.com>
2023-12-13 11:56:54 +0100

    GetRequest Example
    Needs to change to relevant API before merging to main.

--- backend/src/main/java/org/example/Controller/ReviewController.java
+++ backend/src/main/java/org/example/Controller/ReviewController.java
@@ -1,4 +1,31 @@
 package org.example.Controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class ReviewController {
+    public static void getRequest() throws IOException {
+        URL urlForGetRequest = new URL("https://jsonplaceholder.typicode.com/posts/1"); //change maybe
+        String readLine = null;
+        HttpURLConnection connection = (HttpURLConnection) urlForGetRequest.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("userId", "a1bcdef"); //add later
+        int responseCode = connection.getResponseCode();
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+            StringBuffer response = new StringBuffer();
+            while ((readLine = in.readLine()) != null) {
+                response.append(readLine);
+            } in.close();
+            //print out result
+            System.out.println("JSON STRING RESULT " + response.toString());
+        } else {
+            System.out.println("GET: NOT WORKING");
+        }
+    }
 }
--- backend/src/main/java/org/example/Main.java
+++ backend/src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import org.example.Controller.ReviewController;
 
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ReviewController.getRequest();
     }
 }
