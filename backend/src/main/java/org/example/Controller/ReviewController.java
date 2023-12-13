5d54e8471a1090c3674217cbce5ad5367a37dbd2
Arturo Bendezu <artycok123@gmail.com>
2023-12-13 14:11:23 +0100

    Created getReviewForPlace
    Added functionality so the program becomes a server where we can make API calls

--- backend/src/main/java/org/example/Controller/ReviewController.java
+++ backend/src/main/java/org/example/Controller/ReviewController.java
@@ -1,10 +1,16 @@
 package org.example.Controller;
 
+import io.javalin.http.Context;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ReviewController {
     public static void getRequest() throws IOException {
@@ -24,6 +30,28 @@ public class ReviewController {
             } in.close();
             //print out result
             System.out.println("JSON STRING RESULT " + response.toString());
+        } else {
+            System.out.println("GET: NOT WORKING");
+        }
+    }
+    public static void getReviewForPlace(Context ctx) throws IOException, InterruptedException {
+
+        String url = "https://api.yelp.com/v3/businesses/manhatta-new-york/reviews?limit=20&sort_by=yelp_sort";
+        String apiKey = "pOycasf3MSMxdpMNtDrhSdpwuda6fx5osetTYRGwPiY7yLzOOf8Mmi9x6ue_OhiBaIF4hRe2gL_zNsvBZUvv_JB2yQGNkQ4vmUTxNz4wxdWDOCeBgAVnmiVCn-d2ZXYx";
+
+        HttpResponse<String> response;
+        try (HttpClient client = HttpClient.newHttpClient()) {
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .header("Authorization", "Bearer " + apiKey)
+                    .build();
+
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        }
+
+        if (response.statusCode() == 200) {
+            ctx.json(response.body());
         } else {
             System.out.println("GET: NOT WORKING");
         }
--- backend/src/main/java/org/example/Main.java
+++ backend/src/main/java/org/example/Main.java
@@ -1,11 +1,14 @@
 package org.example;
 
+import io.javalin.Javalin;
 import org.example.Controller.ReviewController;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        ReviewController.getRequest();
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ReviewController::getReviewForPlace);
+
     }
 }
