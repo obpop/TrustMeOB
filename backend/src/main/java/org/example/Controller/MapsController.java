6387aa01e90db3f97f9b8aa89ae79d055db5791a
Oliver Berggren <oliverberggren00@gmail.com>
2023-12-29 18:29:52 +0100

    reminder

--- backend/src/main/java/org/example/Controller/MapsController.java
+++ backend/src/main/java/org/example/Controller/MapsController.java
@@ -2,3 +2,8 @@ package org.example.Controller;
 
 public class MapsController {
 }
+
+//ta reda på vad klassen ska retunera
+//ska få in long och lat
+//ska retunera en map från google maps
+//metod som tar in lang och long som arturo kan kalla på
