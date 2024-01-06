abadc483148a8af0c6e708caee060e27600d3f9d
martafranzen <128465268+martafranzen@users.noreply.github.com>
2024-01-06 16:48:03 +0100

    Lade in data från reviews på html

--- backend/src/main/java/org/example/script.js
+++ backend/src/main/java/org/example/script.js
@@ -25,21 +25,27 @@ async function getReviewForPlace() {
 }
 
 function updatePage(data) {
-    document.getElementById("strengths").innerHTML = "<h3>Styrkor</h3><ul>" + data.strengths.map(strength => "<li>" + strength + "</li>").join("") + "</ul>";
-    document.getElementById("flaws").innerHTML = "<h3>Svagheter</h3><ul>" + data.flaws.map(flaw => "<li>" + flaw + "</li>").join("") + "</ul>";
-    document.getElementById("strategy").innerHTML = "<h3>Optimeringsstrategier</h3><ul>" + data.strategy.map(strategy => "<li>" + strategy + "</li>").join("") + "</ul>";
+    // Uppdatera sidan med Google-information
+    const googleData = data.google;
+    document.getElementById("name").innerText = googleData.name
+    document.getElementById("google-address").innerText = "Adress: " + googleData.address;
+    document.getElementById("google-address").innerText = "Recensioner: " + googleData.reviews;
+
+    // Uppdatera sidan med Foursquare-information
+    const foursquareData = data.foursquare;
+    document.getElementById("foursquare-address").innerText = "Recensioner: " + foursquareData.texts;
 }
+
 
 async function testReviews(){
     try {
-        const response = await fetch('http://localhost:8080/places/grandhotel');
+        const response = await fetch('http://localhost:8080/places');
         const data = await response.json();
 
         console.log(data);
+        updatePage(data);
 
     } catch (error) {
         console.error(error);
     }
 }
-
-
